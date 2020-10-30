8dbce68bda080876ee326b1102081ee4795ab721
Nattawat Imthet <60160039@go.buu.ac.th>
2020-10-31 02:31:32 +0700

    Add item to repository

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,5 @@
 const assert = require('assert')
-const request =require('supertest')
+const request = require('supertest')
 const app = require('../index')
 
 
@@ -12,6 +12,16 @@ describe('Validate setting the test', () => {
 describe('GET "/"', () => {
     it('respond with Hello!', (done) => {
         request(app).get('/').expect('Hello!', done)
+    })
+})
+
+describe('GET "/item"', () => {
+    it('respond with array of item', (done) => {
+        let want = [
+            { id: 1, name:'iphone 12'},
+            { id: 2, name:'Nokia 3310'}
+        ]
+        request(app).get('/item').expect(want, done)
     })
 })
 
